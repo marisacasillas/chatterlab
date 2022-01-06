0ca5d00dda551898b40feaf17fe7c16b4920db91
Marisa Casillas <marisacasillas@users.noreply.github.com>
2022-01-06 12:27:41 -0600

    add 4.2 and 5.1 demo snippets

--- D2M2022/demo-snippets.R
+++ D2M2022/demo-snippets.R
@@ -164,17 +164,130 @@ fixeddata2 <- missingdata %>%
   fill(condition)
 
 ################################################################################
-# 4.1
+# 4.2
 
 ## 1994 census data 50K income split
 library(tidyverse)
 adultdata <- read_csv("adult.data.csv")
 
-# let's first just explore the sample and make
-# some data prep changes to make our plots easier to interpret
-unique(adultdata$education)
+ggplot(adultdata, aes(age)) + geom_density()
+ggplot(adultdata, aes(age)) + geom_histogram()
+ggplot(adultdata, aes(age)) + geom_histogram(binwidth = 5)
+
+ggplot(adultdata, aes(`education-num`)) + geom_density()
+ggplot(adultdata, aes(`education-num`)) + geom_density(adjust = 2) 
+
+ggplot(adultdata, aes(race)) + geom_density()
+
+ggplot(adultdata, aes(race)) + geom_bar()
+ggplot(adultdata, aes(race)) + geom_histogram()
+ggplot(adultdata, aes(race)) + geom_histogram(stat = "count")
+
+ggplot(adultdata, aes(occupation)) + geom_bar()
+ggplot(filter(adultdata, occupation != "?"), aes(occupation)) +
+  geom_bar()
+aduldata.occupations.noUNK <- adultdata %>%
+  filter(occupation != "?")
+ggplot(aduldata.occupations.noUNK, aes(occupation)) + geom_bar()
+
+### recreate these plots
+ggplot(adultdata) + geom_histogram(aes(`capital-gain`), binwidth = 5000)
+ggplot(adultdata) + geom_density(aes(y = `hours-per-week`))
+
+################################################################################
+# 5.1
+library(tidyverse)
+
+## One variable
+ggplot(iris, aes(x = Sepal.Length)) +
+  geom_density()
+## Two variables
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) +
+  geom_point()
+## Three variables
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width,
+                 color = Species)) +
+  geom_point()
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width,
+                 shape = Species)) +
+  geom_point()
+## Four variables
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width,
+                 size = Petal.Length)) +
+  geom_point() +
+  facet_grid(~ Species)
+
+
+adultdata <- read_csv("adult.data.csv")
+
+ggplot(adultdata, aes(age, `education-num`)) + geom_point()
+ggplot(adultdata, aes(age, `education-num`)) + geom_jitter()
+ggplot(adultdata, aes(age, `education-num`)) + geom_smooth()
+ggplot(adultdata, aes(age, `education-num`)) +
+  geom_jitter() +
+  geom_smooth()
+ggplot(adultdata, aes(age, `education-num`)) +
+  geom_smooth() +
+  geom_jitter()
+
+# ggplot(adultdata, aes(age, `capital-gain`)) + geom_point()
+# ggplot(adultdata, aes(age, `capital-gain`)) + geom_smooth()
+# ggplot(adultdata, aes(age, `capital-gain`)) +
+#   geom_point() +
+#   geom_smooth()
+# ggplot(filter(adultdata, `capital-gain` < 99999),
+#        aes(age, `capital-gain`)) +
+#   geom_point() +
+#   geom_smooth()
+
+ggplot(adultdata, aes(age, `education-num`)) + geom_boxplot()
+ggplot(adultdata, aes(age, `education-num`, group = age)) +
+  geom_violin()
+ggplot(adultdata, aes(age, `education-num`, group = age)) +
+  geom_jitter() +
+  geom_boxplot()
+ggplot(adultdata, aes(age, `education-num`, group = age)) +
+  geom_jitter(color = "gray50", alpha = 0.1) +
+  geom_boxplot()
+
+ggplot(adultdata, aes(education, workclass)) +
+  geom_count()
+ggplot(adultdata, aes(education, workclass, fill = age)) +
+  geom_raster()
+
+## A few things to do for each variable you plan to use in your
+## plotting/analysis
+
+### Check for NAs
+which(is.na(adultdata$education))
+which(is.na(adultdata$`education-num`))
+which(is.na(adultdata$age))
+for(i in 1:ncol(adultdata)) {print(which(is.na(adultdata[,i])))}
+
+### Look at the data -- NAs are encoded as '?'
+View(adultdata)
+
+### Check that the scale, level, and type fits your expectations
+### for each variable of interest
+head(adultdata)
+table(adultdata$income)
+table(adultdata$education)
+summary(adultdata$age)
+ggplot(adultdata, aes(age)) + geom_density()
+ggplot(adultdata, aes(`capital-gain`)) + geom_density()
+ggplot(adultdata, aes(age, `capital-gain`)) + geom_jitter()
+ggplot(adultdata, aes(age, `capital-loss`)) + geom_jitter()
+
+# let's make some data prep changes to make our plots easier to interpret
 adultdata <- adultdata %>%
   mutate(
+    age.bins = case_when(
+      age <= 35 ~ "<= 35",
+      age > 35 & age <= 50 ~ "36-50",
+      age > 50 & age <= 65 ~ "51-65",
+      age > 65 ~ "66+",
+      TRUE ~ "uh oh"
+    ),
     education.bins = case_when(
       education %in% c("Preschool", "1st-4th",
                        "5th-6th", "7th-8th", "9th", "10th", "11th") ~ 'pre-HS',
@@ -194,14 +307,35 @@ adultdata <- adultdata %>%
     race = factor(race, labels = c("White", "Black", "AsiPacIsl",
                                    "AmerIndFirsNat", "Other")))
 
+ggplot(adultdata, aes(age.bins, `education-num`)) +
+  geom_jitter() +
+  geom_boxplot()
+ggplot(adultdata, aes(age.bins, `education-num`)) +
+  geom_jitter(color = "gray50", alpha = 0.1) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.0)
+
+ggplot(adultdata, aes(education.bins, age)) +
+  geom_jitter(color = "gray50", alpha = 0.1) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.0)
+ggplot(adultdata, aes(education.bins, age)) +
+  geom_jitter(color = "gray50", alpha = 0.1) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.0) +
+  facet_grid(. ~ race)
 ggplot(adultdata, aes(education.bins, age, color = race, fill = race)) +
-  facet_grid(. ~ race) +
-  geom_jitter(alpha = 0.2) +
-  geom_boxplot(outlier.shape = NA, color = "black", alpha = 0.0) # +
-# geom_violin(color = "gray20", alpha = 0.2)
+  geom_jitter(alpha = 0.1) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.0) +
+  facet_grid(. ~ race)
+ggplot(adultdata, aes(education.bins, age, color = race, fill = race)) +
+  geom_jitter(alpha = 0.1) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.0, color = "black") +
+  facet_grid(. ~ race)
 
 # now lets explore how income relates to education, race, and sex
 ggplot(adultdata, aes(education.bins, fill = income)) +
+  geom_bar(position = "fill")
+ggplot(adultdata, aes(age.bins, fill = income)) +
+  geom_bar(position = "fill")
+ggplot(adultdata, aes(sex, fill = income)) +
   geom_bar(position = "fill")
 
 ggplot(adultdata, aes(race, fill = income)) +
@@ -221,6 +355,21 @@ mmdata.long <- read_csv("MM Data.csv") %>%
   pivot_longer(cols = c("Red", "Green", "Blue", "Orange",
                         "Yellow", "Brown"),
                names_to = "Color", values_to = "Number")
+
+ggplot(mmdata.long, aes(Color, Number, fill = Color)) +
+  geom_boxplot()
+ggplot(mmdata.long, aes(Color, Number)) +
+  geom_dotplot(binaxis = "y")
+ggplot(mmdata.long, aes(Color, Number, fill = Color)) +
+  geom_boxplot() +
+  geom_jitter()
+ggplot(mmdata.long, aes(Color, Number, fill = Color)) +
+  geom_boxplot() +
+  geom_dotplot(binaxis = "y")
+
+
+
+## BLABLA for later
 
 ggplot(mmdata.long, aes(Color, Number, color = Color, fill = Color)) +
   scale_color_manual(values = c(
