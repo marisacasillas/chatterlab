7a213456e4748438bf3a53a3ffafb7e6cc63f6b3
Marisa Casillas <marisacasillas@users.noreply.github.com>
2022-03-15 10:26:26 -0500

    improve checks; add shiny app

--- HLI2022/schober_clark_replication/check-eaf.R
+++ HLI2022/schober_clark_replication/check-eaf.R
@@ -1,21 +1,18 @@
 library(tidyverse)
 library(phonfieldwork)
 
-eafs <- list.files("eafs/", "*.eaf")
-interaction.data <- tibble()
-trial.data <- tibble()
-card.data <- tibble()
-
 card.names <- c("monk", "skater", "tower")
 
-for (eaf in eafs) {
-  eaf.data <- eaf_to_df(paste0("eafs/", eaf)) %>%
+check.sc89.eaf <- function(eaf_file, eaf_file_name) {
+  
+  eaf.data <- eaf_to_df(eaf_file) %>%
     # check for valid filename
     mutate(
-      valid.source = grepl("^[a-z]+-[0-9]{8}$", source),
-      ) %>%
+      source = as.character(eaf_file_name),
+      valid.source = grepl("^[A-Za-z]+-[0-9]{8}\\.eaf$", source),
+    ) %>%
     separate(source,
-             into = c("name", "student_id", "file_ext"), sep = "[-.]") %>%
+             into = c("student_name", "student_id", "file_ext"), sep = "[-.]") %>%
     select(-file_ext)
   
   # check for annotations on all the tiers but overhearer_speech
@@ -39,18 +36,19 @@ for (eaf in eafs) {
       valid.pid.name = card.name %in% card.names,
       valid.pid.index = as.numeric(index) < 12
     )
-
-  # print output
-  output = tibble(
-    `valid filename.structure` = eaf.data$valid.source[1],
-    `detected student name` = eaf.data$name[1],
+  
+  # return check outcomes
+  # return(eaf_file_name)
+  return(list(
+    `valid filename structure` = eaf.data$valid.source[1],
+    `detected student name` = eaf.data$student_name[1],
     `detected student id number` = eaf.data$student_id[1],
     `annotations on all critical tiers` = (trial.tier + card.tier +
-      matcher.speech + director.speech + completion.point + 
-      matcher.placement + overhearer.placement) == 7,
+                                             matcher.speech + director.speech + completion.point +
+                                             matcher.placement + overhearer.placement) == 7,
     `valid placement id labels` = !(FALSE %in% placement_ids$valid.pid.name),
-    `valid placement id numbers` = !(FALSE %in% placement_ids$valid.pid.index),
-  )
-  
+    `valid placement id numbers` = !(FALSE %in% placement_ids$valid.pid.index)
+  ))
 }
+
 
