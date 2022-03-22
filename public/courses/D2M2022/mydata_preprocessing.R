c88645d9fb98835e7a3ac546f3653c9ba78906f2
Kennedy Casey <kennedybcasey@gmail.com>
2022-03-21 22:59:48 -0500

    rm public dir

--- public/courses/D2M2022/mydata_preprocessing.R
+++
@@ -1,7 +0,0 @@
-mydata.prepped <- mydata %>%
-  mutate(
-    mpg.x.cyl = mpg * cyl,
-    wt.sqrd = wt^2
-  ) %>%
-  separate(model, c("brand", "model"),
-           sep = " ", extra = "merge", fill = "right")
